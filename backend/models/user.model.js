d6be109ca52d2c1d51e4ad5762c03f955ef428d8
unknown <pritamimca1003@gmail.com>
2025-02-03 14:44:40 +0530

    updated user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -4,6 +4,6 @@ const userSchema = new mongoose.Schema({
     name:{type:String, required:true},
     email:{type:String, required:true, unique:true},
     password:{type:String, required:true}
-})
+},{timestamps:true})
 
 export const User = mongoose.models.User ?? mongoose.model('User',userSchema);
