1d3db0d3a6afb1d736b15d303b622ff27f6c6060
unknown <pritamimca1003@gmail.com>
2025-02-04 13:04:34 +0530

    fixed

--- frontend/lib/auth.ts
+++ frontend/lib/auth.ts
@@ -1,8 +1,11 @@
-export const isAuthenticated = () => {
-  try {
-    if (typeof window === "undefined") return false; // Prevent SSR errors
-    return !!localStorage.getItem("token");
-  } catch (error) {
-    console.log(error);
-  }
-};
+// export const isAuthenticated = () => {
+//   try {
+//     if (typeof window === "undefined") return false; // Prevent SSR errors
+//     return !!localStorage.getItem("token");
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+
+export const isAuthenticated = () => !!localStorage.getItem("token")
