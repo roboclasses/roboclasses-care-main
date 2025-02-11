9fb2f8f289bfd486364c40addbf256ea9679100a
unknown <pritamimca1003@gmail.com>
2025-02-11 16:34:34 +0530

    modified backend and frontend url for production

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,8 @@ const app = express();
 dbConnect();
 
 // middlewares
-app.use(cors());
+// app.use(cors());
+app.use(cors({credentials:true, origin:["https://app-roboclasses-care.up.railway.app"]}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
--- frontend/constants.ts
+++ frontend/constants.ts
@@ -1,6 +1,15 @@
-export const DemoClassUrl = "http://localhost:8002/api/v1/appointments/demoClass";
-export const NormalClassUrl = "http://localhost:8002/api/v1/appointments/normalClass";
-export const NewBatchEntryUrl = "http://localhost:8002/api/v1/newBatchEntries";
-export const AttendanceUrl = "http://localhost:8002/api/v1/attendances";
-export const SignupUrl = "http://localhost:8002/api/v1/auth/signup";
-export const LoginUrl = "http://localhost:8002/api/v1/auth/login";
+// Development
+// export const DemoClassUrl = "http://localhost:8002/api/v1/appointments/demoClass";
+// export const NormalClassUrl = "http://localhost:8002/api/v1/appointments/normalClass";
+// export const NewBatchEntryUrl = "http://localhost:8002/api/v1/newBatchEntries";
+// export const AttendanceUrl = "http://localhost:8002/api/v1/attendances";
+// export const SignupUrl = "http://localhost:8002/api/v1/auth/signup";
+// export const LoginUrl = "http://localhost:8002/api/v1/auth/login";
+
+// Production
+export const DemoClassUrl = "https://api-roboclasses-care.up.railway.app/api/v1/appointments/demoClass";
+export const NormalClassUrl = "https://api-roboclasses-care.up.railway.app/api/v1/appointments/normalClass";
+export const NewBatchEntryUrl = "https://api-roboclasses-care.up.railway.app/api/v1/newBatchEntries";
+export const AttendanceUrl = "https://api-roboclasses-care.up.railway.app/api/v1/attendances";
+export const SignupUrl = "https://api-roboclasses-care.up.railway.app/api/v1/auth/signup";
+export const LoginUrl = "https://api-roboclasses-care.up.railway.app/api/v1/auth/login";
