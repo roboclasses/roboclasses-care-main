0fd44855721d711bce9f2ab1d33e9c17db9149a1
unknown <pritamimca1003@gmail.com>
2025-02-12 11:55:09 +0530

    fixed

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,14 +3,14 @@ import  dotenv from "dotenv"
 dotenv.config();
 
 export const dbConnect = async () => {
-  const dbURL = `mongodb://${process.env.MONGOUSER}:${process.env.MONGOPASSWORD}@${process.env.MONGOHOST}:${process.env.MONGOPORT}/`
+  // const dbURL = `mongodb://${process.env.MONGOUSER}:${process.env.MONGOPASSWORD}@${process.env.MONGOHOST}:${process.env.MONGOPORT}/`
   try {
-    // await mongoose.connect(process.env.MONGO_URI, { dbName: process.env.DB_NAME });
+    await mongoose.connect(process.env.MONGO_URI, { dbName: process.env.DB_NAME });
 
-    await mongoose.connect(dbURL, {dbName: process.env.DB_NAME})
+    // await mongoose.connect(dbURL, {dbName: process.env.DB_NAME})
     console.log("mongodb connected");
   } catch (error) {
-    console.error(error.message);
-    process.exit(1);
+    console.error(error);
+    throw new Error("mongodb failed to connect")
   }
 };
