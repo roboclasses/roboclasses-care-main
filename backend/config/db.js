d78627bfb11a4987a0928090d57e2492bbba2e72
unknown <pritamimca1003@gmail.com>
2025-02-12 17:57:19 +0530

    fix

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,9 +4,9 @@ dotenv.config();
 
 export const dbConnect = async () => {
   try {
-    // await mongoose.connect(process.env.MONGO_URI, { dbName: process.env.DB_NAME });
+    await mongoose.connect(process.env.MONGO_URI, { dbName: process.env.DB_NAME });
 
-    await mongoose.connect(process.env.MONGO_URI)
+    // await mongoose.connect(process.env.MONGO_URI)
     console.log("mongodb connected");
   } catch (error) {
     console.error(error);
