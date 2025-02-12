ed78dccdb31aa80448d138c45aa329ac861cb0eb
unknown <pritamimca1003@gmail.com>
2025-02-12 11:39:16 +0530

    modifed db config for development

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
+import  dotenv from "dotenv"
+dotenv.config();
 
 export const dbConnect = async () => {
+  // const dbURL = process.env.MONGO_URL || `mongodb://${process.env.MONGOUSER}:${process.env.MONGOPASSWORD}@${process.env.MONGOHOST}:${process.env.MONGOPORT}/`
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      dbName: process.env.DB_NAME,
-    });
+    await mongoose.connect(process.env.MONGO_URI, { dbName: process.env.DB_NAME });
 
+    // await mongoose.connect(dbURL, {dbName: process.env.DB_NAME})
     console.log("mongodb connected");
   } catch (error) {
     console.error(error.message);
