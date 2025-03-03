ca443017cdb09d48b94d47e6c4764cb9bd211c92
unknown <pritamimca1003@gmail.com>
2025-03-03 16:20:27 +0530

    modify(middleware, app-sidebar): added routes prefixes and rolebased protected routes; now side bar items conditionally render based on logged in users role.

--- frontend/constants/images.ts
+++ frontend/constants/images.ts
@@ -1,3 +1,3 @@
-export const LOGO_IMG = '/assests/images/logo.svg'
-export const PRIVATE_WALLPAPER = '/assests/images/spikeprime.webp'
-export const PUBLIC_WALLPAPER = '/assests/images/wallpaper.webp'
+export const LOGO_IMG = '/assets/images/logo.svg'
+export const PRIVATE_WALLPAPER = '/assets/images/spikeprime.webp'
+export const PUBLIC_WALLPAPER = '/assets/images/wallpaper.webp'
