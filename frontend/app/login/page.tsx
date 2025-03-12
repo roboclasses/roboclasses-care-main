1c25df2a71887c357a892ddbe7ca6811a5a28bf1
unknown <pritamimca1003@gmail.com>
2025-03-12 15:56:17 +0530

    fix

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,27 +1,29 @@
+import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
 
 import { LoginForm } from "@/demo/login-demo/LoginForm";
 import { LOGO_IMG, PUBLIC_WALLPAPER } from "@/constants/images";
 
 const page = () => {
   return (
-    <div className="grid grid-cols-2">
+    <div className="grid grid-cols-2">  
       <Image
         src={PUBLIC_WALLPAPER}
         width={1200}
         height={1200}
         alt="user-image"
         className="min-h-screen object-cover"
-        />
+      />
       <div className="flex flex-col items-center justify-center gap-5">
-        <Image src={LOGO_IMG} height={200} width={200} alt="robo-class-logo"/>
+        <Image src={LOGO_IMG} height={200} width={200} alt="robo-class-logo" />
         <p className="text-4xl font-bold">Login to your Account</p>
         <LoginForm />
         <div className="flex gap-2 items-center font-bold text-sm">
           <p>Dont have an Account?</p>
-          <Link href="/signup" className="text-lime-600">Signup</Link>
+          <Link href="/signup" className="text-lime-600">
+            Signup
+          </Link>
         </div>
       </div>
     </div>
--- frontend/app/signup/page.tsx
+++ frontend/app/signup/page.tsx
@@ -1,9 +1,9 @@
-import { SignupForm } from "@/demo/signup-demo/SignupForm";
+import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
 
 import { LOGO_IMG, PUBLIC_WALLPAPER } from "@/constants/images";
+import { SignupForm } from "@/demo/signup-demo/SignupForm";
 
 const page = () => {
   return (
@@ -14,14 +14,16 @@ const page = () => {
         height={1200}
         alt="user-image"
         className="min-h-screen object-cover"
-        />
+      />
       <div className="flex flex-col items-center justify-center gap-5">
         <Image src={LOGO_IMG} height={200} width={200} alt="robo-class-logo"/>
         <p className="text-4xl font-bold">Create an Account</p>
         <SignupForm />
         <div className="flex gap-2 items-center font-bold text-sm">
           <p>Already have an Account?</p>
-          <Link href="/login" className="text-lime-600">Login</Link>
+          <Link href="/login" className="text-lime-600">
+            Login
+          </Link>
         </div>
       </div>
     </div>
