86e2d4d9157da58402a534b3a27d20fe6f83f170
Pritam Nandy <pritamimca1003@gmail.com>
2025-03-17 10:13:42 +0530

    fixed

--- frontend/app/manageAttendance/page.tsx
+++ frontend/app/manageAttendance/page.tsx
@@ -3,7 +3,7 @@ import { AttendanceForm } from "@/demo/manage-attendance-demo/AttendanceForm";
 export default function page() {
   return (
       <main className="grid grid-cols-1 p-10">
-        <h1 className="text-4xl font-bold mb-4">Teachers View</h1>
+        <h1 className="text-4xl font-bold mb-4">Manage Attendance</h1>
         <AttendanceForm />
       </main>
   );
