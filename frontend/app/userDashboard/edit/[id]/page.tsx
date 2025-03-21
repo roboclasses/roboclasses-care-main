528ccb292a5045ed28e6e92ee44f6a1623bfb94a
Pritam Nandy <pritamimca1003@gmail.com>
2025-03-21 11:56:51 +0530

    responsiveness

--- frontend/app/userDashboard/edit/[id]/page.tsx
+++ frontend/app/userDashboard/edit/[id]/page.tsx
@@ -15,7 +15,7 @@ const page = () => {
         priority
       />
       <div className="relative z-10 flex items-center justify-center h-full backdrop-blur-sm">
-        <div className="lg:w-full lg:max-w-[600px] w-[400px] lg:mt-5 mt-64 bg-background/95 p-8 m-4 rounded-xl shadow-lg">
+        <div className="lg:w-full lg:max-w-[600px] w-[400px] lg:mt-5 mt-36 bg-background/95 p-8 m-4 rounded-xl shadow-lg">
           <p className="lg:text-4xl text-xl font-bold mb-6 text-center">
             Edit Users
           </p>
--- frontend/app/userDashboard/page.tsx
+++ frontend/app/userDashboard/page.tsx
@@ -2,7 +2,7 @@ import { TableUsers } from '@/demo/manage-roles-demo/TableUsers'
 
 const page = () => {
   return (
-    <div className='p-10'>
+    <div className='p-10 xl:w-[1300px] lg:w-[1000px] md:w-[800px] sm:w-[600px] w-[500px]'>
         <TableUsers />
     </div>
   )
