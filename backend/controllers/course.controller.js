c3213fe88e7f5bedf7f96b5b22d813d276869d07
Pritam Nandy <pritamimca1003@gmail.com>
2025-03-21 13:28:04 +0530

    be updated

---
+++ backend/controllers/course.controller.js
@@ -0,0 +1,79 @@
+import { Course } from "../models/course.model.js";
+
+export const createCourseController = async(req, res)=>{
+    try {
+        const {course} = req.body;
+
+        const courseName = await Course.findOne({course})
+            if(courseName){
+                return res.status(409).json({success:false, message: "Course already exsist."})
+            }
+        
+        const data = await Course.create({course})
+        console.log(data);
+
+        return res.status(201).json({success:true, message:"Course successfully created."})
+         
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({success:false, message:"Internal server error."})     
+    }
+}
+
+export const getCoursesController = async(req, res)=>{
+    try {
+        const data = await Course.find();
+        console.log(data);
+
+        return res.status(200).json(data)
+         
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({success:false, message:"Internal server error."})   
+    }
+}
+
+export const getCourseController = async(req, res)=>{
+    try {
+        const {id} = req.params;
+        const data = await Course.findById(id);
+
+        return res.status(200).json(data)
+        
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({success:false, message:"Internal server error."})  
+        
+    }
+}
+
+export const updateCourseController = async(req, res)=>{
+    try {
+        const {id} = req.params;
+        const courseDetails = req.body;
+
+        const data = await Course.findByIdAndUpdate(id, courseDetails, {new: true})
+        console.log(data);
+
+        return res.status(200).json({success: true, message: "Course successfully updated."})
+          
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({success:false, message:"Internal server error."})  
+        
+    }
+}
+
+export const deleteCourseController = async(req, res)=>{
+    try {
+        const {id} = req.params;
+        const data = await Course.findByIdAndDelete(id);
+
+        return res.status(200).json({success: true, message: "Course successfully deleted."})
+        
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({success:false, message:"Internal server error."})  
+        
+    }
+}
--- backend/routes/newCourseEntryRoute.js
+++ backend/routes/newCourseEntryRoute.js
@@ -1,85 +1,17 @@
 import express from "express";
-import { Course } from "../models/course.model.js";
+import { createCourseController, deleteCourseController, getCourseController, getCoursesController, updateCourseController } from "../controllers/course.controller.js";
 
 const router = express.Router();
-// Create course
-router.post("/courses", async(req,res)=>{
-    try {
-        const {course} = req.body;
 
-        const courseName = await Course.findOne({course})
-        if(courseName){
-            return res.status(409).json({success:false, message: "Course already exsist."})
-        }
+router.post("/courses", createCourseController)
 
-        const data = await Course.create({course})
-        console.log(data);
-        return res.status(201).json({success:true, message:"Course created successfully."})
-        
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({success:false, message:"Internal server error."})    
-    }
-})
+router.get("/courses", getCoursesController)
 
-// Get courses
-router.get("/courses", async(req, res)=>{
-    try {
-        const data = await Course.find();
-        console.log(data);
-        return res.status(200).json(data)
-  
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({success:false, message:"Internal server error."})    
-  
-    }
-})
+router.get("/courses/:id", getCourseController)
 
-// Get course
-router.get("/courses/:id", async(req, res)=>{
-    try {
-        const {id} = req.params;
-        const data = await Course.findById(id)
-        console.log(data);
-        return res.status(200).json(data)
-  
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({success:false, message:"Internal server error."})    
-  
-    }
-})
+router.put("/courses/:id", updateCourseController)
 
-// Update course
-router.put("/courses/:id", async(req, res)=>{
-    try {
-        const {course, numberOfClasses} = req.body;
-        const {id} = req.params;
-        const data = await Course.findByIdAndUpdate(id, {course, numberOfClasses}, {new:true})
-        console.log(data);
-        return res.status(200).json({success:true, message: "Course updated successfully."})
-        
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({success:false, message:"Internal server error."})    
-        
-    }
-})
-
-// Delete course
-router.delete("/courses/:id", async(req, res)=>{
-    try {
-        const {id} = req.params;
-        const data = await Course.findByIdAndDelete(id)
-        console.log(data);
-        return res.status(200).json({success:true, message: "Course deleted successfully."})
-        
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({success:false, message:"Internal server error."})      
-    }
-})
+router.delete("/courses/:id", deleteCourseController)
 
 
 export default router;
