528e23348ca7ba2b733307ecfeedd61d8bf04ce0
Pritam Nandy <pritamimca1003@gmail.com>
2025-03-24 12:54:50 +0530

    responsive

--- frontend/app/userDashboard/page.tsx
+++ frontend/app/userDashboard/page.tsx
@@ -2,7 +2,7 @@ import { TableUsers } from "@/demo/manage-roles-demo/TableUsers";
 
 const page = () => {
   return (
-    <div className="p-10 xl:w-[1300px] lg:w-[1000px] md:w-[800px] sm:w-[600px] w-[500px]">
+    <div className="p-10 2xl:w-full xl:w-[1000px] lg:w-[800px] md:w-[500px] sm:w-[600px] w-[450px]">
       <TableUsers />
     </div>
   );
