344e57d9465c7ecc4ccb86ffd15e992d52537607
Pritam Nandy <pritamimca1003@gmail.com>
2025-03-28 14:53:19 +0530

    work

--- frontend/demo/time-off-demo/TableViewDemo.tsx
+++ frontend/demo/time-off-demo/TableViewDemo.tsx
@@ -22,8 +22,9 @@ import useSWR from "swr";
 import { TimeOffUrl } from "@/constants";
 import { getUserSession } from "@/lib/session";
 import { Button } from "@/components/ui/button";
-import { RefreshCcw } from "lucide-react";
+import { RefreshCcw, Search } from "lucide-react";
 import Cookies from "js-cookie";
+import { Input } from "@/components/ui/input";
 
 
 const fetcher = (url: string) => axios.get(url, {headers:{Authorization: Cookies.get("token")}}).then((res) => res.data);
@@ -35,6 +36,7 @@ const TableViewDemo = () => {
   const [role, setRole] = useState("");
 
   const [filters, setFilters] = useState<{type: string; status: string; fromDate: string}>({type:"", status: "", fromDate: ""})
+  const [searchQuery, setSearchQuery] = useState("")
 
   // Get user session
   useEffect(()=>{
@@ -57,27 +59,32 @@ const TableViewDemo = () => {
       if (filters.type && item.timeOffType !== filters.type) return false;
       if (filters.status && item.status !== filters.status) return false;
       if (filters.fromDate && item.dateRange?.from && new Date(item.dateRange?.from) < new Date(filters.fromDate)) return false;
+      if(searchQuery && !item.teacherName.toLocaleLowerCase().includes(searchQuery.toLocaleLowerCase())
+      && !item.status.toLocaleLowerCase().includes(searchQuery.toLocaleLowerCase())
+      ) return false
       return true;
     });
-  }, [data, filters, role, name]);
+  }, [data, filters, role, name, searchQuery]);
 
 // Reset applied filters
   const resetFilters = () => setFilters({ type: "", status: "", fromDate: "" });
 
     // Handle edge cases
-    if (data?.length === 0) return <div>Empty list for Leaves</div>;
-    if (error instanceof AxiosError){
-      const {message} = error.response?.data;
-      return <div>{message || "An unknown error has occurred."}</div>;
-    } 
-    if (isLoading) return <div>Loading...</div>;
-    if (isValidating) return <div>Refershing data...</div>;
-  
-    
+    const handleEdgeCases=()=>{
+      if (data?.length === 0) return <div>Empty list for Leaves</div>;
+      if (error instanceof AxiosError){
+        const {message} = error.response?.data;
+        return <div>{message || "An unknown error has occurred."}</div>;
+      } 
+      if (isLoading) return <div>Loading...</div>;
+      if (isValidating) return <div>Refershing data...</div>;
+    }
+
+ 
     return (
-      <Card className="lg:w-full w-[400px]">
+      <Card className="lg:w-full w-full">
         <CardHeader className="flex flex-row items-center justify-between">
-          <div className="flex items-center gap-2">
+          <div className="flex items-center gap-2 mr-2">
             <FilterTimeOffDemo onFilterLeaves={setFilters} />
             {Object.values(filters).some(Boolean) && (
               <Button variant="outline" className="rounded-full shadow-none" onClick={resetFilters}>
@@ -86,8 +93,13 @@ const TableViewDemo = () => {
               </Button>
             )}
           </div>
+          <div className="flex lg:w-full w-[300px] max-w-sm items-center border border-gray-300 rounded-lg px-2 py-1">
+            <Search className="h-4 w-4 mr-2.5" />
+            <Input type="search" placeholder="Search Teacher/Status..." className="w-full border-0" value={searchQuery} onChange={(e)=>setSearchQuery(e.target.value)}/>
+          </div>
         </CardHeader>
         <CardContent className="lg:w-full w-[400px] overflow-x-auto">
+          {handleEdgeCases()}
           <Table>
             <TableCaption>A list of past leaves</TableCaption>
             <TableHeader>
