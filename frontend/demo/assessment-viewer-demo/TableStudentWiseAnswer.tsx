2a81782f190a3eab2c5d518a2fbb029363836c5f
Pritam Nandy <pritamimca1003@gmail.com>
2025-05-23 13:33:47 +0530

    fix

--- frontend/demo/assessment-viewer-demo/TableStudentWiseAnswer.tsx
+++ frontend/demo/assessment-viewer-demo/TableStudentWiseAnswer.tsx
@@ -74,11 +74,11 @@ export function TableStudentWiseAnswer() {
   return (
     <div>
     <Table className="border border-black">
-      <TableCaption>A list of answers given by students</TableCaption>
+      <TableCaption>A list of marks given to students</TableCaption>
       <TableHeader>
         <TableRow>
+          <TableHead className="w-[100px]">Batch Name</TableHead>
           <TableHead className="w-[100px]">Candidate Name</TableHead>
-          <TableHead className="w-[100px]">Subject</TableHead>
           <TableHead className="w-[100px]">Level</TableHead>
           <TableHead className="w-[100px]">Submission Time</TableHead>
           <TableHead>Out Of</TableHead>
@@ -88,8 +88,8 @@ export function TableStudentWiseAnswer() {
       <TableBody>
         {answerData?.map((ans: AnswerType) => (
           <TableRow key={ans._id}>
-            <TableCell className="font-medium">{ans.candidate}</TableCell>
             <TableCell className="font-medium">{ans.batch}</TableCell>
+            <TableCell className="font-medium">{ans.candidate}</TableCell>
             <TableCell className="font-medium">{ans.assessmentLevel}</TableCell>
             <TableCell className="font-medium">{ans.submissionTime ? format(new Date(ans.submissionTime), 'PPpp') : ''}</TableCell>
             <TableCell className="font-medium">{ assessmentData.find(
