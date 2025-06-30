66fe5dd0975a71097f324317c0df117a235b8e84
Pritam Nandy <pritamimca1003@gmail.com>
2025-06-30 20:09:30 +0530

    fix

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
     name:{type:String, required:true},
     email:{type:String, required:true, unique:true},
     password:{type:String, required:true},
-    role:{type:String, enum:["student","teacher","admin","constructor"], required:true},
+    role:{type:String, enum:["student","teacher","admin","contractor"], required:true},
     workingHours:{type:String, default:""},
     workingDays:{type:String, default:""}
 },{timestamps:true})
