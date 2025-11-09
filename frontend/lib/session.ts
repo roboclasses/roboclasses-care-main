8f49c79d29f09fd89078974b43cd5e595f306462
Pritam Nandy <pritamimca1003@gmail.com>
2025-11-09 20:40:56 +0530

    fix

--- frontend/lib/session.ts
+++ frontend/lib/session.ts
@@ -5,7 +5,7 @@ import { cookies } from "next/headers";
 const cookie ={
     options:{ httponly: true, secure: false, samesite: "lax", path: '/' },
     // options:{ httponly: true, secure: process.env.NODE_ENV === "production", samesite: process.env.NODE_ENV === "production"? "None" : "Lax", path: '/' },
-    duration:10 * 24 * 60 * 60 * 1000, //10 days
+    duration:20 * 24 * 60 * 60 * 1000, //20 days
 
 }
 // user session will expires in 10days
