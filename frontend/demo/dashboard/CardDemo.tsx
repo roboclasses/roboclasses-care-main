7dc9d394c2bfebe1ff267d592fff5a47d9cb8b56
Pritam Nandy <pritamimca1003@gmail.com>
2026-05-19 10:16:26 +0530

    fix

--- frontend/demo/dashboard/CardDemo.tsx
+++ frontend/demo/dashboard/CardDemo.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { ROBO_ROMP_YT } from "@/constants/videos";
 
@@ -6,7 +6,7 @@ const CardDemo = () => {
   return (
     <Card className="space-x-2 flex flex-col items-center">
       <CardContent className="p-4">
-          <iframe
+        {/* <iframe
             src={ROBO_ROMP_YT}
             width={500}
             height={300}
@@ -14,7 +14,19 @@ const CardDemo = () => {
             sandbox="allow-scripts allow-same-origin allow-presentation"
             loading="lazy"
             className="rounded-xl lg:w-[500px] w-[300px]"
-          />
+          /> */}
+
+        <iframe
+          width="560"
+          height="315"
+          src={ROBO_ROMP_YT}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+          className="rounded-xl lg:w-125 w-75"
+        ></iframe>
       </CardContent>
       <CardFooter>
         <p className="lg:text-xl font-bold">Who are we?</p>
